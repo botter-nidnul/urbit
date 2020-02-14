a170b17a646784ad4a637e1e6e18e7740564eed4
botter-nidnul <55599302+botter-nidnul@users.noreply.github.com>
2020-02-14 07:18:47 -0600

    change urbit files to build only aarch64

--- nix/release.nix
+++ nix/release.nix
@@ -10,8 +10,7 @@ let
       deps = crossdeps env;
     };
 
-  linux64 = release "linux64" nixcrpkgs.linux64;
-  darwin  = release "darwin"  nixcrpkgs.mac;
+  linux-arm64 = release "linux-arm64" nixcrpkgs.linux-arm64;
 
   ent = env:
     import ./pkgs/ent/cross.nix env;
@@ -44,6 +43,5 @@ let
 in
 
 {
-  linux64 = builds-for-platform linux64;
-  darwin  = darwin_extra // builds-for-platform darwin;
+  linux-arm64 = builds-for-platform linux-arm64;
 }
